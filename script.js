ffb1d36136a132cfe79d4fbd07b1a6b371fe4d8c
aweoluwatobi <aweoluwatobi26@gmail.com>
2022-03-03 18:21:57 +0100

    Completed game logic and refactored code

--- script.js
+++ script.js
@@ -1,75 +1,70 @@
 'use strict';
 
-function random() {
-  return Math.floor(Math.random() * (6 - 1 + 1) + 1);
-}
-
 const newGameBtn = document.querySelector('.btn-new');
 const diceImg = document.querySelector('.dice');
 const rollDiceBtn = document.querySelector('.btn-roll');
 const holdBtn = document.querySelector('.btn-hold');
 const player1 = document.querySelector('.player-1');
 const player2 = document.querySelector('.player-2');
-// const currentScore1 = document.getElementById('current-1');
-// const currentScore2 = document.getElementById('current-2');
 let playerScore = [0, 0];
 let currentScore = 0;
 let activePlayer = 1;
 
-let player1Score = 0;
-let player2Score = 0;
+// Set active player current score
+const setCurrentScore = currScore => {
+  document.getElementById(`current-${activePlayer}`).textContent = currScore;
+};
 
-// switch to next player when dice roll becomes one or hold button is pressed.
+// Set active player main score
+const setPlayerScore = mainScore => {
+  document.getElementById(`score-${activePlayer}`).textContent = mainScore;
+};
 
-function diceRollFunc() {
-  const dice = random();
+//Change background color
+const setBackgroundColor = bgColor => {
+  document.querySelector(`.player-${activePlayer}`).style.backgroundColor =
+    bgColor;
+};
+//Set text Color
+const setTextColor = color => {
+  document.getElementById(`name-${activePlayer}`).style.color = color;
+};
+
+//When dice is rolled, generate random number and display image
+function rollDice() {
+  const dice = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   diceImg.src = `images/dice-${dice}.png`;
   diceImg.classList.remove('hidden');
 
+  // switch to next player when dice is one. Keep adding dice value to current score if dice is not 1
   if (dice === 1) {
     currentScore = 0;
-    document.getElementById(`current-${activePlayer}`).textContent =
-      currentScore;
+    setCurrentScore(currentScore);
     activePlayer = activePlayer === 1 ? 2 : 1;
     player1.classList.toggle('player-active');
     player2.classList.toggle('player-active');
   } else {
     currentScore += dice;
-    document.getElementById(`current-${activePlayer}`).textContent =
-      currentScore;
+    setCurrentScore(currentScore);
   }
 }
 
-rollDiceBtn.addEventListener('click', diceRollFunc);
+//dice is rolled when button is clicked
+rollDiceBtn.addEventListener('click', rollDice);
 
+// switch to next player turn when hold button is pressed
 holdBtn.addEventListener('click', function () {
-  if (activePlayer === 1) {
-    playerScore[0] += currentScore;
-    document.getElementById(`score-${activePlayer}`).textContent =
-      playerScore[0];
-    currentScore = 0;
-    document.getElementById(`current-${activePlayer}`).textContent =
-      currentScore;
-    player1Score += playerScore[0];
-  } else {
-    playerScore[1] += currentScore;
-    document.getElementById(`score-${activePlayer}`).textContent =
-      playerScore[1];
-    currentScore = 0;
-    document.getElementById(`current-${activePlayer}`).textContent =
-      currentScore;
-    player2Score += playerScore[1];
-  }
+  playerScore[`${activePlayer}` - 1] += currentScore;
+  setPlayerScore(playerScore[`${activePlayer}` - 1]);
+  currentScore = 0;
+  setCurrentScore(currentScore);
 
+  // First player to score 100 wins and game ends
   if (playerScore[`${activePlayer}` - 1] >= 100) {
     diceImg.classList.add('hidden');
-    rollDiceBtn.removeEventListener('click', diceRollFunc);
-    document.querySelector(`.player-${activePlayer}`).style.backgroundColor =
-      '#383838';
-    document.getElementById(`name-${activePlayer}`).style.color =
-      'rgb(207, 45, 107)';
-
-    TODO; // SET TEXT COLOR AND OTHERS WHEN SCORE IS GREATER THAN OR EQUAL TO 100
+    rollDiceBtn.removeEventListener('click', rollDice);
+    setBackgroundColor('#383838');
+    setTextColor('rgb(207, 45, 107)');
   } else {
     activePlayer = activePlayer === 1 ? 2 : 1;
     player1.classList.toggle('player-active');
@@ -77,18 +72,18 @@ holdBtn.addEventListener('click', function () {
   }
 });
 
+// Refresh game when the button is pressed. All values is reset to default
 newGameBtn.addEventListener('click', function () {
   playerScore = [0, 0];
   currentScore = 0;
+  activePlayer = 1;
   document.getElementById('score-1').textContent = playerScore[0];
   document.getElementById('score-2').textContent = playerScore[1];
-  document.getElementById('current-1').textContent = currentScore;
-  document.getElementById('current-2').textContent = currentScore;
+  setCurrentScore(currentScore);
   diceImg.classList.add('hidden');
   player1.classList.add('player-active');
   player2.classList.remove('player-active');
-  document.querySelector(`.player-${activePlayer}`).style.backgroundColor = '';
-  document.getElementById(`name-${activePlayer}`).style.color = '';
-  activePlayer = 1;
-  rollDiceBtn.addEventListener('click', diceRollFunc);
+  setBackgroundColor('');
+  setTextColor('');
+  rollDiceBtn.addEventListener('click', rollDice);
 });
